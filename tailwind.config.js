b712a95040453603615a282ce204bf99d873f161
gartholiver <garth@gartholiver.io>
2022-01-06 14:30:11 -0500

    standardized tailwind defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     './inc/theme/filters.php',
   ],
   theme: {
+    colors: {
+      'black': '#000000',
+      'white': '#ffffff',
+      'transparent': 'transparent',
+      'current': 'currentColor',
+      'red': {
+        'DEFAULT': '#c32529',
+      },
+    },
     extend: {},
   },
   plugins: [],
